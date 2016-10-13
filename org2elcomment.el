31993b14e5d227867b4e6df8c2f5cafb33278735
Junpeng Qiu <qjpchmail@gmail.com>
2016-10-13 17:28:00 -0400

    Refactor code
    Reference: #9

--- org2elcomment.el
+++ org2elcomment.el
@@ -78,16 +78,14 @@
 (require 'pulse nil t)
 
 (defvar org2elcomment-backend 'ascii)
+(defvar org2elcomment-exporter 'org2elcomment-default-exporter)
 
 (defvar org2elcomment-last-source nil)
 (make-variable-buffer-local 'org2elcomment-last-source)
 
 (defvar org2elcomment-anywhere-last-source nil)
 
-(defvar org2elcomment-anywhere-readme nil)
-(make-variable-buffer-local 'org2elcomment-anywhere-readme)
-
-(defvar org2elcomment-readme-exporter 'org2elcomment--export-readme)
+(defvar org2elcomment-anywhere-org-file nil)
 
 (defun org2elcomment--find-bounds (buffer)
   (let (beg end)
@@ -103,43 +101,62 @@
               (message "org2elcomment: No \";;; Commentary:\" or \";;; Code:\" found.")
               nil)))))))
 
-;;;###autoload
-(defun org2elcomment-anywhere (el-file &optional readme-file)
-  (interactive
-   (list
-    (let ((prompt
-           (if org2elcomment-anywhere-last-source
-               (format "Elisp file (default \"%s\"): "
-                       org2elcomment-anywhere-last-source)
-             "Elisp file: ")))
-      (setq org2elcomment-anywhere-last-source
-            (read-file-name prompt
-                            nil
-                            org2elcomment-anywhere-last-source
-                            t)))))
-  (let* ((el-file-dir (file-name-directory el-file)))
+(defun org2elcomment--get-prompt (initial default-value)
+  (if default-value
+      (format "%s (default \"%s\"): " initial default-value)
+    (format "%s: " initial)))
+
+(defmacro org2elcomment--interactive-form (name)
+  `(interactive
+    (list (let ((prompt (org2elcomment--get-prompt "Elisp file" ,name)))
+            (setq ,name (read-file-name prompt nil ,name t))))))
+
+(defun org2elcomment-default-exporter (org-file)
+  (with-temp-buffer
+    (insert-file-contents org-file)
+    (org-export-as org2elcomment-backend)))
+
+(defun org2elcomment--save-org-to-el (buffer value)
+  "Set current file's local variable `org2elcomment-anywhere-org-file'."
+  (when (yes-or-no-p "Save org file location to Elisp file? ")
+    (with-current-buffer buffer
+      (add-file-local-variable
+       'org2elcomment-anywhere-org-file value))))
+
+(defun org2elcomment--read-org-from-el (buffer el-file-dir)
+  "Get org file location from current buffer's context.
+
+`org2elcomment-anywhere-org-file' only save the relative path of
+the org file. We need to return its abosolute path based
+directory EL-FILE-DIR."
+  (with-current-buffer buffer
+    (hack-local-variables)
+    (when (assoc 'org2elcomment-anywhere-org-file
+                 file-local-variables-alist)
+      (let ((org-file
+             (concat (file-name-as-directory el-file-dir)
+                     org2elcomment-anywhere-org-file)))
+        (and org-file (file-exists-p org-file) org-file)))))
+
+(defun org2elcomment--update-comment (el-file org-file &optional pre-handler
+                                              post-handler)
+  (if (file-locked-p el-file)
+      (message "org2elcomment: File %S has been locked. Operation denied. " el-file)
     (with-temp-buffer
       (insert-file-contents el-file)
+
       (emacs-lisp-mode)
 
-      ;; Load local-variable `org2elcomment-anywhere-readme' from current buffer
-      (hack-local-variables)
+      ;; change `org-file' if necessary
+      (when pre-handler
+        (setq org-file (funcall pre-handler el-file org-file)))
 
-      (goto-char (point-min))
-      (setq readme-file
-            (or (if (and readme-file (file-exists-p readme-file))
-                    readme-file)
-                (org2elcomment--get-readme-setting (current-buffer) el-file-dir)
-                (read-file-name "README.org file: " nil nil t)))
-
-      (unless (org2elcomment--get-readme-setting (current-buffer) el-file-dir)
-        (org2elcomment--save-readme-setting
-         (current-buffer) (file-relative-name readme-file el-file-dir)))
+      ;; now it's good to update `org2elcomment-anywhere-org-file'
+      (setq org2elcomment-anywhere-org-file org-file)
 
       (let ((bounds (org2elcomment--find-bounds (current-buffer)))
-            (output (funcall org2elcomment-readme-exporter readme-file))
-            beg)
-        (when (and bounds output)
+            output beg end)
+        (when (and bounds (setq output (funcall org2elcomment-exporter org-file)))
           (kill-region (car bounds) (cdr bounds))
           (goto-char (car bounds))
           (insert "\n")
@@ -147,90 +164,59 @@
           (insert output)
           (comment-region beg (point))
           (insert "\n")
+          (setq end (point))
           (setq output (buffer-string))
-          (if (file-locked-p el-file)
-              (message "org2elcomment: File %S has been locked. Operation denied. " el-file)
-            (let ((buffer (get-file-buffer el-file)))
-              (if buffer
-                  (with-current-buffer buffer
-                    (let ((point (point)))
-                      (erase-buffer)
-                      (insert output)
-                      (goto-char point))
-                    (message "org2elcomment: The commentary in buffer %S has been updated! "
-                             (buffer-name buffer)))
-                (write-region (point-min)
-                              (point-max) el-file)
-                (message "org2elcomment: The commentary of file %S have been updated! " el-file)))))))))
+          (let ((buffer (get-file-buffer el-file)))
+            (if buffer
+                (with-current-buffer buffer
+                  (let ((point (point)))
+                    (erase-buffer)
+                    (insert output)
+                    (goto-char point))
+                  (message "org2elcomment: The commentary in buffer %S has been updated."
+                           (buffer-name buffer)))
+              (write-region (point-min)
+                            (point-max) el-file)
+              (message "org2elcomment: The commentary of file %S has been updated." el-file))
+            ;; some visual effect if necessary
+            (when post-handler
+              (funcall post-handler (find-file-noselect el-file) beg end))))))))
 
-(defun org2elcomment--export-readme (readme-file)
-  (let (output)
-    (with-temp-buffer
-      (insert-file-contents readme-file)
-      (setq output
-            (org-export-as org2elcomment-backend)))
-    output))
+(defun org2elcomment--pre-handler (el-file org-file)
+  (goto-char (point-min))
+  (let ((el-file-dir (file-name-directory el-file)))
+    (setq org-file
+          (or (if (and org-file (file-exists-p org-file))
+                  org-file)
+              (org2elcomment--read-org-from-el (current-buffer) el-file-dir)
+              (read-file-name
+               (org2elcomment--get-prompt "Org file" org2elcomment-anywhere-org-file)
+               el-file-dir
+               org2elcomment-anywhere-org-file
+               t)))
 
-(defun org2elcomment--save-readme-setting (buffer value)
-  "Set current file's local variable `org2elcomment-readme'."
-  (when (yes-or-no-p "Save README.org setting to elisp file? ")
-    (with-current-buffer buffer
-      (add-file-local-variable
-       'org2elcomment-anywhere-readme value))))
+    (unless (org2elcomment--read-org-from-el (current-buffer) el-file-dir)
+      (org2elcomment--save-org-to-el
+       (current-buffer) (file-relative-name org-file el-file-dir)))
+    org-file))
 
-(defun org2elcomment--get-readme-setting (buffer el-file-dir)
-  "Get readme setting from current buffer's context.
-
-`org2elcomment-anywhere-readme' only save README.org's file name
-we need to return its full path based directory EL-FILE-DIR."
-  (with-current-buffer buffer
-    (let ((readme-file
-           (when org2elcomment-anywhere-readme
-             (concat (file-name-as-directory el-file-dir)
-                     org2elcomment-anywhere-readme))))
-      (when (and readme-file
-                 (file-exists-p readme-file))
-        readme-file))))
+(defun org2elcomment--post-handler (el-buf beg end)
+  (switch-to-buffer el-buf)
+  (push-mark)
+  (goto-char beg)
+  (recenter 0)
+  (when (featurep 'pulse)
+    (pulse-momentary-highlight-region beg end)))
 
 ;;;###autoload
-(defun org2elcomment (file-name)
-  (interactive
-   (list
-    (let ((prompt
-           (if org2elcomment-last-source
-               (format "Source file (default \"%s\"): "
-                       org2elcomment-last-source)
-             "Source file: ")))
-      (setq org2elcomment-last-source (read-file-name prompt
-                                                      nil
-                                                      org2elcomment-last-source
-                                                      t)))))
-  (let* ((src-buf (find-file-noselect file-name))
-         (bounds (org2elcomment--find-bounds src-buf))
-         (output (org-export-as org2elcomment-backend))
-         beg end)
-    (when bounds
-      (progn
-        (with-current-buffer src-buf
-          (kill-region (car bounds) (cdr bounds))
-          (save-excursion
-            (goto-char (car bounds))
-            (insert "\n")
-            (setq beg (point))
-            (insert output)
-            (comment-region beg (point))
-            (insert "\n")
-            (setq end (point))))
-        (switch-to-buffer src-buf)
-        (push-mark)
-        (goto-char (car bounds))
-        (recenter 0)
-        (when (featurep 'pulse)
-          (pulse-momentary-highlight-region (car bounds) end))))))
+(defun org2elcomment-anywhere (el-file &optional org-file)
+  (org2elcomment--interactive-form org2elcomment-anywhere-last-source)
+  (org2elcomment--update-comment el-file org-file #'org2elcomment--pre-handler))
+
+;;;###autoload
+(defun org2elcomment (el-file)
+  (org2elcomment--interactive-form org2elcomment-last-source)
+  (org2elcomment--update-comment el-file (buffer-file-name) nil #'org2elcomment--post-handler))
 
 (provide 'org2elcomment)
 ;;; org2elcomment.el ends here
-
-;; Local Variables:
-;; org2elcomment-anywhere-readme: "README.org"
-;; End:
